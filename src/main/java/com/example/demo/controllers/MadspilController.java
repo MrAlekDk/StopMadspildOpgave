109f3e4cc53e644934d000882df1bc006b3a0fbc
Alexander <alex030t@stud.kea.dk>
2021-03-09 15:49:48 +0100

    Moved folder location

--- src/main/java/com/example/demo/controllers/MadspilController.java
+++ src/main/java/com/example/demo/controllers/MadspilController.java
@@ -1,4 +1,16 @@
 package com.example.demo.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MadspilController {
+
+
+
+    @GetMapping(value="/")
+    public String homePage(){
+
+        return "index.html";
+    }
 }
