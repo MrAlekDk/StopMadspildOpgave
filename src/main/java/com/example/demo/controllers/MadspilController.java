e8fb863d6adc7fd09596ff18ddb898008e770a7e
mettemariehws <mettemhws@gmail.com>
2021-03-12 13:11:44 +0100

    Har lavet html kode til køleskabet

--- src/main/java/com/example/demo/controllers/MadspilController.java
+++ src/main/java/com/example/demo/controllers/MadspilController.java
@@ -6,10 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MadspilController {
 
-
-
     @GetMapping(value="/")
     public String homePage(){
         return "index.html";
+    }
+
+    @GetMapping(value="food-suggestions")
+    public String foodSuggestions() {
+       return "redirect:/food-suggestions";
     }
 }
