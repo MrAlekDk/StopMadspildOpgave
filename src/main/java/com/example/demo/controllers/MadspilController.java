f8e8fd537a6feadab4ce29c2994b451473cdd2d2
mettemariehws <mettemhws@gmail.com>
2021-03-12 14:06:16 +0100

    Har lavet html kode til køleskabet og der virker fandeme

--- src/main/java/com/example/demo/controllers/MadspilController.java
+++ src/main/java/com/example/demo/controllers/MadspilController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MadspilController {
@@ -18,4 +20,18 @@ public class MadspilController {
         return "Info.html";
     }
 
+    @GetMapping(value="/køleskab")
+    public String køleskab() {
+        return "køleskab.html";
+    }
+
+    @PostMapping(value="/køleskab-post")
+    public String køleskab(Model model) {
+        return "redirect:/skal-du-have-mad";
+    }
+
+    @GetMapping("/skal-du-have-mad")
+    public String foodSuggestions() {
+        return "food-suggestions.html";
+    }
 }
